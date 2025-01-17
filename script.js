e7e0541df2c1f18f359fc5df3d96684ecbb4fc65
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-18 02:31:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+
+function getCart(){
+	return JSON.parse(sessionStorage.getItem("cart")) || [];
+}
+
+function saveCart(cart) {
+	sessionStorage.setItem("cart",JSON.stringify(cart));
+}
 
 // Render product list
 function renderProducts() {
@@ -19,20 +28,62 @@ function renderProducts() {
     li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
+
+  document.querySelectorAll(".add-to-cart-btn").forEach((button)=>{
+	button.addEventListener('click',(e)=>{
+		const prodId=parseInt(e.target.getAttribute("data-id"));
+		addToCart(prodId);
+	});
+  });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	let cart=getCart();
+	cartList.innerHTML='';
+
+	cart.forEach((item)=>{
+		const li = document.createElement("li");
+        li.innerHTML = `${item.name} - $${item.price} <button class="remove-from-cart-btn" data-id="${item.id}">Remove</button>`;
+        cartList.appendChild(li);
+	});
+
+	document.querySelectorAll('.remove-from-cart-btn').forEach((item)=>{
+		item.addEventListener('click', (e)=> {
+			let prodId=parseInt(e.target.getAttribute('data-id'));
+			removeFromCart(prodId);
+		})
+	});
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	const prod=products.find((p)=>p.id===productId);
+
+	if(!prod) return;
+
+	const cart = getCart();
+	cart.push(prod);
+	saveCart(cart);
+	renderCart();
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	let cart=getCart();
+
+	cart=cart.filter((p)=>p.id!==productId);
+	saveCart(cart);
+	renderCart();
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+	sessionStorage.removeItem("cart");
+	renderCart();
+}
 
 // Initial render
 renderProducts();
 renderCart();
+document.getElementById('clear-cart-btn').addEventListener('click', clearCart);
